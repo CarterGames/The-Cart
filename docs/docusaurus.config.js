5f3aa29021f9d05713cc83aa2ba2c5eb72747011
J <jonathan@carter.games>
2026-03-25 19:55:21 +0000

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,12 +23,13 @@ const config = {
   url: 'https://cartergames.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/The-Cart/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'CarterGames', // Usually your GitHub org/user name.
   projectName: 'The-Cart', // Usually your repo name.
+  deploymentBranch: "gh=pages",
   trailingSlash: false,
   onBrokenLinks: 'throw',
 
