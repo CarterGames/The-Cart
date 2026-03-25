7e51ecbf706710247b0ad7051b9a230a80858806
J <jonathan@carter.games>
2026-03-25 19:27:01 +0000

    🚧 Config update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cartergames.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/TheCart/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'carter games', // Usually your GitHub org/user name.
-  projectName: 'the cart', // Usually your repo name.
+  organizationName: 'CarterGames', // Usually your GitHub org/user name.
+  projectName: 'TheCart', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
